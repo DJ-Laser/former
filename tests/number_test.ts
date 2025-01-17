83cd5b9f458681e18968687ddccfb7910f734033
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-01-16 20:55:31 -0500

    first commit

---
+++ tests/number_test.ts
@@ -0,0 +1,41 @@
+import { assertEquals } from "@std/assert";
+import { Selector } from "../src/mod.ts";
+
+Deno.test(function defaultTest() {
+  const selector = new Selector(["a", "b", "c"]).number();
+  assertEquals(selector.items, ["1. a", "2. b", "3. c"]);
+});
+
+Deno.test(function startingNumberTest() {
+  const selector = new Selector(["a", "b", "c"]).number({ start: 5 });
+  assertEquals(selector.items, ["5. a", "6. b", "7. c"]);
+});
+
+Deno.test(function positiveStepTest() {
+  const selector = new Selector(["a", "b", "c"]).number({ step: 2 });
+  assertEquals(selector.items, ["1. a", "3. b", "5. c"]);
+});
+
+Deno.test(function zeroStepTest() {
+  const selector = new Selector(["a", "b", "c"]).number({ step: 0 });
+  assertEquals(selector.items, ["1. a", "1. b", "1. c"]);
+});
+
+Deno.test(function negativeStepTest() {
+  const selector = new Selector(["a", "b", "c"]).number({ step: -1 });
+  assertEquals(selector.items, [" 1. a", " 0. b", "-1. c"]);
+});
+
+Deno.test(function zeroSpacingTest() {
+  const selector = new Selector(["a", "b", "c"]).number({
+    spacing: "zero",
+    step: 50,
+  });
+
+  assertEquals(selector.items, ["001. a", "051. b", "101. c"]);
+});
+
+Deno.test(function defaultCheckTest() {
+  const selector = new Selector(["a", "b", "c"]).number({ step: -1 });
+  assertEquals(selector.items, [" 1. a", " 0. b", "-1. c"]);
+});
